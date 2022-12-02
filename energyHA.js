c68fea964b074a2a64309f8352553c74f6571bdb
José <jose.cboliveira@outlook.pt>
2022-12-02 09:10:52 +0000

    Initial

---
+++ energyHA.js
@@ -0,0 +1,85 @@
+const sendToHA1 = require("./sendenergyHA")
+const fs = require('fs')
+const csv = require('csvtojson')
+XLSX = require('xlsx');
+
+const dir = fs.opendirSync('.')
+let dirent
+
+var files_in = [];
+files_in = fs.readdirSync('./');
+files_in = files_in.sort()
+files_in.forEach(element => {
+    var nameTemp = element;
+    if (element.substring(element.length - 4, element.length) == 'xlsx') {
+
+        const workBook = XLSX.readFile(nameTemp);
+        const CVSFile = nameTemp.substring(0, nameTemp.length - 4);
+        XLSX.writeFile(workBook, CVSFile + 'csv', { bookType: "csv" });
+
+
+        fs.readFile(CVSFile + 'csv', 'utf8', function (err, data) {
+
+            if (err) {
+                return console.log(err);
+            }
+
+            for (i = 0; i < 8; ++i) {
+                data = data.split("\n").slice(1).join("\n")
+            }
+
+            data = data.replace(RegExp('/', 'g'), '-');
+
+            var regex = /.*-.*-.*,/g, result, dates = [], values = [], values = [];
+
+            while ((result = regex.exec(data))) {
+                data = data.replaceAt(result.index + 10, 'T');
+            }
+            csv({
+                noheader: true,
+                output: "csv"
+            })
+                .fromString(data)
+                .then((csvRow) => {
+                    var t = 0, j = 0;
+                    var temp = 0.0;
+                    var jsonDataVar = [];
+                    var dateTemp = new Date(csvRow[0][0]);
+                    var initDate = new Date(dateTemp.setMinutes(dateTemp.getMinutes() - 15 - 60)).toISOString()
+
+                    // var Init = new Date(csvRow [0][0]).toISOString();
+                    var dateAnt = new Date().now;
+                    for (i = 0; i < csvRow.length; ++i) {
+                        temp = temp + Number(csvRow[i][1]);
+                        var date = new Date(csvRow[i][0]);
+                        date = new Date(date.setHours(date.getHours() - 1));
+                        var dateISOString = date.toISOString()
+                        let minutes = date.getMinutes();
+
+                        if (minutes == 0) {
+                            if (dateAnt != date) {
+                                dateAnt = date;
+                                var dataEnergy = {
+                                    start: dateISOString,
+                                    sum: temp
+                                };
+                                jsonDataVar.push(dataEnergy)
+                                t = 0;
+                            }
+                        }
+
+                        ++t;
+
+                    }
+                    console.log('Data inicial:' + initDate);
+                    sendToHA1.sendToHA(jsonDataVar, initDate);
+
+                })
+        });
+    }
+})
+dir.close();
+
+String.prototype.replaceAt = function (index, replacement) {
+    return this.substring(0, index) + replacement + this.substring(index + replacement.length);
+}
---
+++ sendenergyHA.js
@@ -0,0 +1,112 @@
+const W3CWebSocket = require("websocket").w3cwebsocket; // npm install websocket
+
+const host = "192.168.50.230";
+const port = 8123;
+const protocol = "ws"; // ws or wss if ssl
+
+// long lived access token from profile > Create Token
+const access_token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJmZDMyOTAyNTkxZTA0MWZhYjc4NDUyMDBiY2I0MGJiYyIsImlhdCI6MTY2OTgwMDQ1MCwiZXhwIjoxOTg1MTYwNDUwfQ.eRRZBoyfTRo3uKvZ-W9WzxtXKwbRjslT1ncOVgOYtOU";
+const entityName = "e-redes";
+const entityId = "energy_consumption_kwh";
+
+// datetime must be in ISO 8601 format with timezone
+// stats = [{start: '2022-01-01T00:00:00z', sum: 0},...]
+// start_search_time = datetime from which to search for old total
+exports.sendToHA = function (stats, start_search_time) {
+  const client = new W3CWebSocket(
+    `${protocol}://${host}:${port}/api/websocket`
+  );
+
+  let id = 1;
+  const awaiting = new Set();
+
+  client.onerror = function (e) {
+    console.log("Connection Error", e);
+  };
+
+  client.onopen = function () {
+    console.log("WebSocket Client Connected");
+  };
+
+  client.onclose = function () {
+    console.log("WebSocket Client Closed");
+  };
+  
+  client.onmessage = function (e) {
+    try {
+      const data = JSON.parse(e.data);
+      if (data.type === "auth_required") {
+        client.send(
+          JSON.stringify({
+            type: "auth",
+            access_token,
+          })
+        );
+      }
+      if (data.type === "auth_ok") {
+        // get the last state sum
+        const payload = {
+          id,
+          type: "history/statistics_during_period",
+          start_time: start_search_time,
+          end_time: stats[0].start,
+          statistic_ids: [`external:${entityId}`],
+          period: "hour",
+        };
+        console.log("request last stats");
+        awaiting.add(id);
+        client.send(JSON.stringify(payload));
+        id++;
+      }
+      if (data.type === "auth_invalid") {
+        console.log("Auth invalid", data);
+        client.close();
+      }
+      if (data.type === "result") {
+        awaiting.delete(data.id);
+        if (!data.success) {
+          console.log("Error", data.error);
+        } else {
+          console.log("Success", data.id);
+          // collect answer and insert updated stats
+          if (data.id === 1) {
+            const results = data.result[`external:${entityId}`];
+            if (results && results.length > 0) {
+              const lastStat = results[results.length - 1];
+              const sum = lastStat.sum;
+              console.log("Last stat", lastStat);
+              // shift all data fo a unique grand total, since last_rest does not work
+              stats.forEach((stat) => {
+                stat.sum += sum;
+              });
+            } else {
+              console.log("No last stats");
+            }
+            const payload = {
+              id,
+              type: "recorder/import_statistics",
+              metadata: {
+                has_mean: false,
+                has_sum: true,
+                name: entityName,
+                source: "external",
+                statistic_id: `external:${entityId}`,
+                unit_of_measurement: "kWh",
+              },
+              stats,
+            };
+            console.log("sending stats");
+            awaiting.add(id);
+            client.send(JSON.stringify(payload));
+            id++;
+          }
+        }
+        if (awaiting.size === 0) {
+          client.close();
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+}
